b9e74922b87b2ceb0316f60299fb0653b7e81f4d
Dominick Hemphill <oncereal1@gmail.com>
2020-03-01 12:36:02 -0500

    Initial commit:  completed lab 7 task and created gitIgnore to ignore settings

---
+++ src/edu/cscc/RPSLSpock.java
@@ -0,0 +1,79 @@
+package edu.cscc;
+import java.util.Random;
+
+/**
+ * Rock, Paper, Scissors, Lizard, & Spock
+ * Human input validator and Computer selection creator class
+ * @author Dom Hemphill
+ * @version 1.0
+ */
+public class RPSLSpock {
+
+    // not sure how this calculates a random
+    static Random rand = new Random(System.currentTimeMillis());
+
+    public static final String ROCK = "rock";
+    public static final String PAPER = "paper";
+    public static final String SCISSORS = "scissors";
+    public static final String LIZARD = "lizard";
+    public static final String SPOCK = "spock";
+
+    /**
+     * Determines if user has made a valid selection
+     * @param pick
+     * @return true or false
+     */
+    public static boolean isValidPick(String pick) {
+        if (pick == null) {
+            return false;
+        }
+        pick = pick.trim();
+        return (ROCK.equalsIgnoreCase(pick) ||
+                PAPER.equalsIgnoreCase(pick) ||
+                SCISSORS.equalsIgnoreCase(pick) ||
+                LIZARD.equalsIgnoreCase(pick) ||
+                SPOCK.equalsIgnoreCase(pick));
+    }
+
+    /**
+     * Generates random computer selection
+     * @return (computer generated) pick
+     */
+    public static String generatePick() {
+        String pick = null;
+        switch (rand.nextInt(5)) {
+            case 0:
+                pick = ROCK;
+                break;
+            case 1:
+                pick = PAPER;
+                break;
+            case 2:
+                pick = SCISSORS;
+                break;
+            case 3:
+                pick = LIZARD;
+                break;
+            case 4:
+                pick = SPOCK;
+                break;
+        }
+        return pick;
+    }
+
+    /**
+     * Determines if the computer won or not
+     * @param c_pick
+     * @param h_pick
+     * @return true or false
+     */
+    public static boolean isComputerWin(String c_pick,String h_pick) {
+        // why didn't c pick have to .toLowerCase?
+        h_pick = h_pick.toLowerCase();
+        return ((ROCK.equals(c_pick) && (SCISSORS.equals(h_pick) || LIZARD.equals(h_pick))) ||
+                (PAPER.equals(c_pick) && (ROCK.equals(h_pick) || SPOCK.equals(h_pick))) ||
+                (SCISSORS.equals(c_pick) && (PAPER.equals(h_pick) || LIZARD.equals(h_pick))) ||
+                (LIZARD.equals(c_pick) && (PAPER.equals(h_pick) || SPOCK.equals(h_pick))) ||
+                (SPOCK.equals(c_pick) && (ROCK.equals(h_pick) || SCISSORS.equals(h_pick))));
+    }
+}
